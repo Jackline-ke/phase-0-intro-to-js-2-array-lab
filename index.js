bfadc04a8967c024868e392ea69c3e6271a5e0f4
Jackline-ke <antoninahodhiambo@gmail.com>
2023-03-12 08:33:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = [
+    "Milo", "Otis", "Garfield"
+];
+//console.log(cats);
+
+function destructivelyAppendCat(name){
+   cats.push(name);
+};
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+ };
+function destructivelyRemoveFirstCat(name){
+    cats.shift();
+ };
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+ };
+ function  appendCat(name){
+    return [...cats, name];
+ };
+ function  prependCat(name){
+    return [name, ...cats];
+ };
+ function removeLastCat(name){
+    return cats.slice(0, 2);
+ };
+ function removeFirstCat(name){
+    return cats.slice(1);
+ };
